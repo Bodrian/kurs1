c0108187214e20f0b6308215837b7ece8708d17f
Bodrian <u3363777@yandex.ru>
2022-01-25 09:02:20 +0300

    ds

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from yandex import Yandex
 from vk import Vk
 
 if __name__ == '__main__':
-    tokenvk = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-    tokenya = 'AQAAAAABEN4eAADLW1EFaLIPUEB5gyhWoL7lX9M'
+    tokenvk = ''
+    tokenya = ''
     file_path = 'filename.json' #имя JSON файла
 
     #работа с API VK
